6897f7d437aa751864579e3d01ba5cb7447522e1
Anatolii Petrov <hello@aswitch.me>
2024-12-08 19:24:32 +0100

    Appstore Requirements fix

--- MissMatch/View/ContactPermissonView.swift
+++ MissMatch/View/ContactPermissonView.swift
@@ -14,13 +14,16 @@ struct ContactPermissonView: View {
     @State private var isNotGranted = false
     @State private var showNextView = false
     @State private var buttonTapped = false
+    @State private var showAlert = false
+    @State private var isUserNotAgree = false
     
     var body: some View {
         ZStack {
             Color(colorScheme == .dark ? Color.black : Color.white)
+            
             if isNotGranted {
                 VStack {
-                    Text("Without contacts access, the app won't work :(")
+                    Text("Without contacts access, the app won't work🥺 Also, for the best User Experience turn on notifications.")
                         .font(.title)
                         .foregroundStyle(.secondary)
                         .bold()
@@ -32,39 +35,107 @@ struct ContactPermissonView: View {
                         Text("Ok, take me to settings")
                             .bold()
                             .padding()
-                            .foregroundStyle(.cyan) .background(Color(UIColor.systemBackground))
+                            .foregroundStyle(.cyan)
+                            .background(Color(UIColor.systemBackground))
                             .cornerRadius(8)
                             .shadow(color: colorScheme == .dark ? Color.white.opacity(0.3) : Color.gray, radius: 3, x: 0, y: 2)
                     }
                     .padding(.top, 10)
                 }
-            } else {
-                    VStack {
-                        Text("Firstly, we need your permission to access your contacts and send notifications. To ensure your data is secure, we only use hashed phone numbers, not names or other details. Your privacy is our priority. ")
-                            .font(.title)
-                            .foregroundStyle(.secondary)
+            } else if isUserNotAgree {
+                VStack {
+                    Text("We can't proceed without your consent to process your contacts securely☹️ Please grant permission to continue.")
+                        .font(.title)
+                        .foregroundStyle(.secondary)
+                        .bold()
+                        .padding(20)
+                    
+                    Button(action: {
+                        showAlert.toggle()
+                        withAnimation {
+                            buttonTapped = true
+                        }
+                    }) {
+                        Text("Grant Permission")
                             .bold()
                             .padding()
-                        Button(action: {
-                            requestContactAccess()
-                            withAnimation {
-                                buttonTapped.toggle()
-                            }
-                        }) {
-                            Text("Sounds good, Let’s Go")
-                                .bold()
-                                .padding()
-                                .background(Color(UIColor.systemBackground))
-                                .foregroundStyle(Color(hex: "3AD60F"))
-                                .cornerRadius(8)
-                                .shadow(color: colorScheme == .dark ? Color.white.opacity(0.3) : Color.gray, radius: 3, x: 0, y: 2)
-                        }
-                        
+                            .foregroundStyle(.cyan)
+                            .background(Color(UIColor.systemBackground))
+                            .cornerRadius(8)
+                            .shadow(color: colorScheme == .dark ? Color.white.opacity(0.3) : Color.gray, radius: 3, x: 0, y: 2)
                     }
-                    .animation(.easeInOut(duration: 1), value: buttonTapped)
-                    .blur(radius: buttonTapped ? 7 : 0)
+                    
+                    Button(action: {
+                        if let url = URL(string: "https://asw1tch.github.io/umissme-privacy-policy.html") {
+                            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                        }
+                    }) {
+                        Text("View Privacy Policy")
+                            .bold()
+                            .padding()
+                            .foregroundStyle(.cyan)
+                            .background(Color(UIColor.systemBackground))
+                            .cornerRadius(8)
+                            .shadow(color: colorScheme == .dark ? Color.white.opacity(0.3) : Color.gray, radius: 3, x: 0, y: 2)
+                    }
+                    .padding(.top, 10)
+                }
+                .blur(radius: buttonTapped ? 7 : 0)
+                .alert("Consent Required", isPresented: $showAlert) {
+                    Button("Agree") {
+                        requestContactAccess()
+                        withAnimation {
+                            buttonTapped = true
+                        }
+                    }
+                    Button("Cancel", role: .cancel) {
+                        isUserNotAgree = true
+                        buttonTapped = false
+                    }
+                } message: {
+                    Text("By continuing, you agree that we will process your contacts securely by encrypted phone numbers and using them to find matches. No other details, like names, will be shared or uploaded.")
+                }
+            } else {
+                VStack {
+                    Text("Firstly, we need your permission to access your contacts and send notifications. To ensure your data is secure, we only use encrypted phone numbers, not names or other details. Your privacy is our priority.")
+                        .font(.title)
+                        .foregroundStyle(.secondary)
+                        .bold()
+                        .padding()
+                    
+                    Button(action: {
+                        showAlert = true
+                        withAnimation {
+                            buttonTapped = true
+                        }
+                    }) {
+                        Text("Sounds good, Let’s Go")
+                            .bold()
+                            .padding()
+                            .background(Color(UIColor.systemBackground))
+                            .foregroundStyle(Color(hex: "3AD60F"))
+                            .cornerRadius(8)
+                            .shadow(color: colorScheme == .dark ? Color.white.opacity(0.3) : Color.gray, radius: 3, x: 0, y: 2)
+                    }
+                }
+                .animation(.easeInOut(duration: 1), value: buttonTapped)
+                .blur(radius: buttonTapped ? 7 : 0)
                 .fullScreenCover(isPresented: $showNextView) {
                     SignInView(signInVM: SignInViewModel())
+                }
+                .alert("Consent Required", isPresented: $showAlert) {
+                    Button("Agree") {
+                        requestContactAccess()
+                        withAnimation {
+                            buttonTapped = true
+                        }
+                    }
+                    Button("Cancel", role: .cancel) {
+                        isUserNotAgree = true
+                        buttonTapped = false
+                    }
+                } message: {
+                    Text("By continuing, you agree that we will process your contacts securely by encrypted phone numbers and using them to find matches. No other details, like names, will be shared or uploaded.")
                 }
             }
         }
@@ -76,6 +147,7 @@ struct ContactPermissonView: View {
         store.requestAccess(for: .contacts) { granted, error in
             DispatchQueue.main.async {
                 if granted {
+                    isUserNotAgree = false
                     requestNotificationPermission()
                 } else {
                     isNotGranted = true
@@ -99,7 +171,7 @@ struct ContactPermissonView: View {
                 print("Notification permission granted")
             } else {
                 isNotGranted = true
-                print("Notification permission denied")
+                print("Notification permission not granted")
             }
         }
     }
